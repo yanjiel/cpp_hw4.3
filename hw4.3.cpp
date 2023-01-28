a895ccd82a2283e53d143e2f9bdecfe0a67aaa30
yanjiel <yanjieliu829@gmail.com>
2023-01-28 10:53:53 -0600

    3rd commit

--- hw4.3.cpp
+++ hw4.3.cpp
@@ -1,3 +1,6 @@
+//end() always returns a special, past - the - end iterator.
+//That iterator is not dereferenceable(it doesn't point to any element), it just serves as an indicator of the container's end.
+
 #include<algorithm>
 #include<iostream>
 #include<string>
@@ -19,5 +22,8 @@ int main() {
 	maxlenftn maxf;
 	for_each(names.begin(), names.end(), maxf);
 	cout << maxf.maxlen << endl;
+
+	for_each(names.begin(), names.end(), [](string& v) {static string n = "-"; v = n; });
+	for_each(names.begin(), names.end(), [](string v) {cout << v << " "; });
 	return 0;
 }
